5de85cb53d44937edfaed2af1d63f1b4a9df055f
Tomasz Kiljańczyk <39602955+Gunock@users.noreply.github.com>
2019-11-21 13:56:00 +0100

    Finished adding main script

--- test/test_icdReader.py
+++ test/test_icdReader.py
@@ -1,12 +1,12 @@
 """Unit tests for IcdReader class"""
 from unittest import TestCase
 
-from icd_reader.classes.IcdReader import IcdReader
+from icd_reader.classes.IcdWikipediaMapper import IcdWikipediaMapper
 
 
 class TestIcdReader(TestCase):
     def test_get_disease_wikipedia_data(self):
-        icd_reader: IcdReader = IcdReader("../icd_reader/resources/codeSpaces.json")
+        icd_reader: IcdWikipediaMapper = IcdWikipediaMapper("../icd_reader/resources/codeSpaces.json")
         title, url, pol_url = icd_reader.get_disease_wikipedia_data("E10.3")
 
         self.assertEqual("Diabetic retinopathy", title)
@@ -14,7 +14,7 @@ class TestIcdReader(TestCase):
         self.assertEqual("https://pl.wikipedia.org/wiki/Retinopatia_cukrzycowa", pol_url)
 
     def test_get_diseases_wikipedia_data(self):
-        icd_reader: IcdReader = IcdReader("../icd_reader/resources/codeSpaces.json")
+        icd_reader: IcdWikipediaMapper = IcdWikipediaMapper("../icd_reader/resources/codeSpaces.json")
         diseases: list = ["P14.1", "D51.9", "E10.3", "M07", "L10.0", "L10.3", "L20.9", "P10.4", "ZZ100"]
         data: list = icd_reader.get_diseases_wikipedia_data(diseases)
         self.assertEqual(len(diseases) - 2, len(data))
